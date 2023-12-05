f4c3af2eb27d319c1a082cdb8f50b0b7cdd3adbc
charo <gc.keita3@isepdiamniadio.edu.sn>
2023-12-05 15:34:11 +0100

    commit du 05/12/2023

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import {  OnInit } from '@angular/core';
 
- var  header;
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-    ngOnInit(): void {
-      
+    ngOnInit() {
+      document.addEventListener("DOMContentLoaded", function() {
+        var header = document.querySelector("Header");
+    
+     window.addEventListener("scroll", function() {
+            if(window.scrollY > 100) {
+                header?.classList.add("scrolled");
+            } else {
+                header?.classList.remove("scrolled");
+             }
+        });
+     });
     }
 }
+
+
--- src/assets/Javascripte/scripte.js
+++ src/assets/Javascripte/scripte.js
@@ -1,11 +1,30 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var header = document.getElementById("#head");
+//  document.addEventListener("DOMContentLoaded", function() {
+//     var header = document.getElementById("#header");
 
-    window.addEventListener("scroll", function() {
-        if(this.window.scrollY > 100) {
-            header.classList.add("scolled");
-        } else {
-            header.classList.remove("scolled");
-        }
-    });
-});
+//  window.addEventListener("scroll", function FixerHeader() {
+//         if(this.window.scrollY > 100) {
+//             header.classList.add("scolled");
+//         } else {
+//             header.classList.remove("scolled");
+//          }
+//     });
+//  });
+
+
+// // Sélection du header
+// const header = document.getElementById('#header');
+
+// // Récupération de la position du header
+// const headerOffset = header.offsetTop;
+
+// // Fonction pour fixer le header
+// function fixerHeader() {
+//   if (window.scrollY > 100) {
+//     header.classList.add('.header');
+//   } else {
+//     header.classList.remove('.header');
+//   }
+// }
+
+// // Écoute du scroll de la page
+// window.addEventListener('scroll', fixerHeader);
